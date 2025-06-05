802ed16e94bd38edb8c9ea36e598ead2b5433381
MD Lijanur Rahman Limon <139367973+vip-limon-gamer@users.noreply.github.com>
2025-06-05 20:53:55 +0600

    Update call.js

--- Script/commands/call.js
+++ Script/commands/call.js
@@ -25,7 +25,7 @@ module.exports.run = async ({ api, event, args }) => {
  api.unsendMessage(info.messageID);
  }, 90000);
  
- return api.sendMessage(`✅ —͟͟͞͞𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 কল বোম্বিং সম্পন্ন হয়েছে ${number} নম্বরে।`, event.threadID, event.messageID);
+ return api.sendMessage(`✅ ✿✦ 𝗡𝗔𝗛𝗜𝗗𝗔 ✦✿  | ʟʀ ʟɪᴍᴏɴ কল বোম্বিং সম্পন্ন হয়েছে ${number} নম্বরে।`, event.threadID, event.messageID);
  } catch (error) {
  return api.sendMessage(`❌ ত্রুটি: ${error.message}`, event.threadID, event.messageID);
  }
